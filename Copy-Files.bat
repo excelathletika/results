24da38cd6840ce9d2fe351fe914954f63e24158e
excelathletika <lee@mntribe.net>
2023-06-10 09:59:26 -0600

    Auto-committed on 2023-06-10

--- Copy-Files.bat
+++ Copy-Files.bat
@@ -3,4 +3,3 @@ REM Simple batch file to post results online
 REM Used to push resutls
 
 XCOPY C:\realtime C:\MeetResultsWeb\Meets\2023\TrialsMeet  /s
-RENAME C:\MeetResultsWeb\Meets\2023\TrialsMeet\index.htm C:\:\MeetResultsWeb\Meets\2023\TrialsMeet\index.html  
