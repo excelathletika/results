82f8af408568f38fa13c4d616c80dafc49cb4112
excelathletika <lee@mntribe.net>
2023-06-11 08:48:14 -0600

    Auto-committed on 2023-06-11

--- Copy-Files.bat
+++
@@ -1,5 +0,0 @@
-echo off
-REM Simple batch file to post results online
-REM Used to push resutls
-
-XCOPY C:\realtime C:\MeetResultsWeb\Meets\2023\TrialsMeet  /s
--- Post-Meets-Online.bat
+++ Post-Meets-Online.bat
@@ -1,10 +1,29 @@
 @echo off
-REM Simple batch file to post results online
-REM Used to push resutls
+REM Simple batch file to post results online.
+REM First we copy the files from C:\realtime the directory where
+REM Meet Manager posts the results locally.  Then we use GIT to
+REM post the results online to a GIT Repo.
+REM Important - The target directory must exist to be successful
 
+set MEET_PATH="C:\MeetResultsWeb\Meets\"
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 
-%GIT_PATH% add -A
-%GIT_PATH% commit -am "Auto-committed on %date%"
-%GIT_PATH% push -u origin main
+REM ============================================================
+REM Change this to the year and directory that you wish to post results
+REM locally on your computer.
+set MEET_YEAR="2023"
+set MEET_CURRENT_DIR="TrialsMeet"
+REM ============================================================
+
+set TARGET=%MEET_PATH%\%MEET_YEAR%\%MEET_CURRENT_DIR%\
+
+If exist %TARGET% (
+  XCOPY C:\realtime %TARGET%   /s /e /k /y
+  %GIT_PATH% add -A
+  %GIT_PATH% commit -am "Auto-committed on %date%"
+  %GIT_PATH% push -u origin main
+  ECHO "Exists"
+) else (
+  ECHO You need to create the target Meet directory: %TARGET%
+)
 
