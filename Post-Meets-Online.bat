17b9814be633d3c4c4476b185a6a17b6ef34be4e
Excel Athletika <136122466+excelathletika@users.noreply.github.com>
2023-07-20 15:53:49 -0600

    Update Post-Meets-Online.bat

--- Post-Meets-Online.bat
+++ Post-Meets-Online.bat
@@ -3,8 +3,8 @@ REM Simple batch file to post results online.
 REM First we copy the files from C:\realtime the directory where
 REM Meet Manager posts the results locally.  Then we use GIT to
 REM post the results online to a GIT Repo.
-REM Important - The target directory must exist to be successful
 
+REM Establish default values 
 set MEET_PATH="C:\MeetResultsWeb\Meets\results\Meets\"
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 
@@ -14,7 +14,6 @@ REM locally on your computer.
 set MEET_YEAR="2023"
 set MEET_CURRENT_DIR="YourMeetName"
 REM ============================================================
-
 SET TARGET=%MEET_PATH%\%MEET_YEAR%\%MEET_CURRENT_DIR%\
 
 REM Pull any changes made to the remote to ensure we are in sync.
@@ -25,8 +24,10 @@ If not exist %TARGET% (
   ECHO Creating target location...
   MKDIR %TARGET%
 )
+
 ECHO Copying files from HyTek...
 XCOPY C:\realtime %TARGET%   /s /e /k /y /q
+
 ECHO Pushing results to the web...
 %GIT_PATH% add -A
 %GIT_PATH% commit -am "Auto-committed on %date%"
